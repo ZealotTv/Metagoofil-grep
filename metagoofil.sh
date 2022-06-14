169461028f096cb907e8267181bd6133ec763218
ZealotTv <72410423+ZealotTv@users.noreply.github.com>
2022-06-14 09:25:50 +0300

    Add files via upload

---
+++ metagoofil.sh
@@ -0,0 +1,14 @@
+cd way/to/your/folder/with/metagoofil
+python metagoofil.py  -d $1 -t doc,docx,pdf,ppt,pptx,odf,xls,xlsx -l 400 -n 400 -o $2  -w 
+filetypes=("doc" "docx" "pdf" "ppt" "pptx" "odf" "xls" "xlsx")
+fields=("Author" "Creator" "Email" "Producer" "Template")
+cd $2
+
+for i in "${fields[@]}"
+do
+	echo $i >> $3
+    for j in "${filetypes[@]}"
+    do
+	    exiftool -r *.$j | egrep -i $i | sort -u >> $3
+    done
+done
